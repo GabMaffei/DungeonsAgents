61a81b5ae5ce29d65b0d3fcb53c968d900f14533
FragasPrisma <andrefragas14@gmail.com>
2023-10-24 00:24:21 -0300

    First commit

--- core/src/com/mygdx/dungeonsagents/ai/Orc.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.dungeonsagents.ai;
-
-import com.mygdx.dungeonsagents.Entity;
-
-public class Orc extends Entity {
-    public Orc(boolean ally, int placement, int fightingClass, float viewportWidth, float viewportHeight) {
-        super(ally, placement, fightingClass, viewportWidth, viewportHeight);
-    }
-}
